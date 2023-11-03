bfc0389755a828780aff8b6da38deb8ce06b54a2
joshu2761 <joshua.y0761@gmail.com>
2023-11-03 14:15:13 -0400

    added some spacing

--- Sketch.java
+++ Sketch.java
@@ -56,10 +56,12 @@ public class Sketch extends PApplet {
     int intY = 0;
 
     // Draws a 30x30 grid of white squares
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = 0; intColumn < 30; intColumn ++){
+        
         intX = 3 + intRow * 10;
         intY = 300 + 3 + intColumn * 10;
+
         fill(255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -72,14 +74,17 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+
     int intX = 0;
     int intY = 0;
 
     // Alternates between black and white columns
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = 0; intColumn < 30; intColumn ++){
+
         intX = 300 + 3 + intRow * 10;
         intY = 300 + 3 + intColumn * 10;
+
         fill((intRow + 1) % 2 * 255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -92,14 +97,17 @@ public class Sketch extends PApplet {
    * Don't use multiple 'if' statements.
    */
   public void draw_section3(){
+
     int intX = 0;
     int intY = 0;
 
     // Alternates between black and white rows
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = 0; intColumn < 30; intColumn ++){
+
         intX = 600 + 3 + intRow * 10;
         intY = 300 + 3 + intColumn * 10;
+
         fill(intColumn % 2 * 255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -111,18 +119,21 @@ public class Sketch extends PApplet {
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+
     int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = 0; intColumn < 30; intColumn ++){
         intX = 900 + 3 + intRow * 10;
         intY = 300 + 3 + intColumn * 10;
+
         fill(0);
         // If the row is divisible by 2 and the column - 1 is divisible by 2 then the squares are white
         if (intRow % 2 == 0 && (intColumn + 1) % 2 == 0) {
           fill(255);
         }
+
         noStroke();
         rect(intX, intY, 5, 5);
       }
@@ -133,14 +144,17 @@ public class Sketch extends PApplet {
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
+
     int intX = 0;
     int intY = 0;
 
     // Draws a pyrimid of white squares
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = (30 - intRow - 1); intColumn < 30; intColumn++ ){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = (30 - intRow - 1); intColumn < 30; intColumn ++){
+
         intX = 3 + intRow * 10;
         intY = 3 + intColumn * 10;
+
         fill(255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -149,14 +163,17 @@ public class Sketch extends PApplet {
   }
 
   public void draw_section6(){
+
     int intX = 0;
     int intY = 0;
 
     // Draws the same pyrimid but turned the other way
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = intRow; intColumn < 30; intColumn++ ){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = intRow; intColumn < 30; intColumn ++){
+
         intX = 300 + 3 + intRow * 10;
         intY = 3 + intColumn * 10;
+
         fill(255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -165,14 +182,17 @@ public class Sketch extends PApplet {
   }
 
   public void draw_section7(){
+
     int intX = 0;
     int intY = 0;
 
     // Draws upside down pyrimid
-    for(int intRow = 0; intRow <= 30; intRow++){
-      for(int intColumn = (30 - intRow - 1); intColumn >= 0; intColumn-- ){
+    for(int intRow = 0; intRow <= 30; intRow ++){
+      for(int intColumn = (30 - intRow - 1); intColumn >= 0; intColumn --){
+
         intX = 600 + 3 + intRow * 10;
         intY = 3 + intColumn * 10;
+
         fill(255);
         noStroke();
         rect(intX, intY, 5, 5);
@@ -181,14 +201,17 @@ public class Sketch extends PApplet {
   }
   
   public void draw_section8(){
+
     int intX = 0;
     int intY = 0;
 
     // Draws upside down pyrimid but facing different way
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = intRow; intColumn >= 0; intColumn-- ){
+    for(int intRow = 0; intRow < 30; intRow ++){
+      for(int intColumn = intRow; intColumn >= 0; intColumn --){
+
         intX = 900 + 3 + intRow * 10;
         intY = 3 + intColumn * 10;
+
         fill(255);
         noStroke();
         rect(intX, intY, 5, 5);
